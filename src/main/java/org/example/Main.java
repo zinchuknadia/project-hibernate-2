acedbd88061afeaa07592777d6033a1c184d1f31
zinchuknadia <nadiia.zinchuk.oi.2023@lpnu.ua>
2025-12-22 22:02:21 +0200

    add Properties and sessionFactory configuration

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.example.domain.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    private final SessionFactory sessionFactory;
+
+    public Main() {
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/test_schema");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "12345678");
+        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        properties.put(Environment.HBM2DDL_AUTO, "validate");
+
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Address.class)
+                .addAnnotatedClass(Category.class)
+                .addAnnotatedClass(City.class)
+                .addAnnotatedClass(Country.class)
+                .addAnnotatedClass(Customer.class)
+                .addAnnotatedClass(Film.class)
+                .addAnnotatedClass(FilmText.class)
+                .addAnnotatedClass(Inventory.class)
+                .addAnnotatedClass(Language.class)
+                .addAnnotatedClass(Payment.class)
+                .addAnnotatedClass(Rental.class)
+                .addAnnotatedClass(Staff.class)
+                .addAnnotatedClass(Store.class)
+                .addProperties(properties)
+                .buildSessionFactory();
+    }
+    public static void main( String[] args )
+    {
+        Main main = new Main();
+    }
+}
